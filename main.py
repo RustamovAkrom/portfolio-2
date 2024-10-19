705bda276315b6a2e1e7a049e21699951557b313
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-19 19:32:50 +0500

    fix: change deploy

--- main.py
+++ main.py
@@ -7,31 +7,19 @@ from app import create_app
 logging.basicConfig(level=logging.DEBUG)
 
 
-def init_deploy_configurations(app: Flask):
-    @app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('mysite')
-            origin = repo.remotes.origin
-            origin.pull()
-            return 'Updated PythonAnywhere successfully', 200
-        else:
-            return "Wrong event type", 400
-        
-        
-def main():
-    try:
-        app = create_app().run(
-            host="127.0.0.1",
-            port=8000,
-            debug=True
-        )
-        init_deploy_configurations(app)
+app = create_app().run(
+    host="127.0.0.1",
+    port=8000,
+    debug=True
+)
 
-        
-    except KeyboardInterrupt:
-        exit(1)
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('mysite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return "Wrong event type", 400
 
-
-if __name__=='__main__':
-    main()
