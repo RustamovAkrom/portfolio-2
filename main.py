77c2a70d3c341f33635577d59078f57045ebd8f6
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-19 19:21:28 +0500

    add deploying conf

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+from flask import Flask, json, request
+import logging
+import git
 from app import create_app
 
 
+logging.basicConfig(level=logging.DEBUG)
+
+
+def init_deploy_configurations(app: Flask):
+    @app.route('/update_server')
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('mysite')
+            origin = repo.remotes.origin
+            origin.pull()
+            return 'Updated PythonAnywhere successfully', 200
+        else:
+            return "Wrong event type", 400
+        
+        
 def main():
     try:
-        create_app().run(
+        app = create_app().run(
             host="127.0.0.1",
             port=8000,
             debug=True
         )
+        init_deploy_configurations(app)
+
+        
     except KeyboardInterrupt:
         exit(1)
 
