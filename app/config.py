5b768d43518c50dfd39c2bec7afde3f7bb596e02
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-20 16:03:09 +0500

    add security

--- app/config.py
+++ app/config.py
@@ -1,36 +1,31 @@
 import os
 
+from dotenv import load_dotenv
+
 
 class BaseConfig:
+    load_dotenv()
+
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    SECRET_KEY = "<SECRET KEY>"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///sqlite3.db"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
+    DEBUG = os.getenv("DEBUG")
     UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "pdf", "txt"}
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587  # обычно это 587 для TLS или 465 для SSL
 
-    # Настройки email для отправки через Gmail
-    MAIL_USERNAME = "akromjonrustamov56@gmail.com"
-    MAIL_PASSWORD = "frcygugpgjcehvnf"
+    # configure email to optimzate Gmail
+    MAIL_USERNAME = os.getenv("EMAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_DEFAULT_SENDER = "akromjonrustamov56@gmail.com"
 
-    # Замените на ваш Redis URI
-    CELERY_BROKER_URL = "sqla+sqlite:///tasks.sqlite"
-    CELERY_RESULT_BACKEND = "db+sqlite:///results.sqlite"
-    CELERY_ACCEPT_CONTENT = ["json"]
-    CELERY_TASK_SERIALIZER = "json"
-    CELERY_RESULT_SERIALIZER = "json"
-    CELERY_TIMEZONE = "UTC"
-
     # Caching pages
     CACHE_TYPE = "SimpleCache"
     CACHE_DEFAULT_TIMEOUT = 300
-    
 
 
 class DevelopmentConfig(BaseConfig):
@@ -40,7 +35,6 @@ class DevelopmentConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
 config = DevelopmentConfig()
